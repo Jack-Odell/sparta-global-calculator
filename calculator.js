70eddbfaea27b6f6ed416944ebe10b811c9c0e74
Tech-A48 <tech-a48@Tech-A48s-MacBook-Air.local>
2018-04-23 17:56:12 +0100

    Added basic calculator functionality. Addition, Subtraction, Division, Multiplication and Modulus

--- calculator.js
+++ calculator.js
@@ -1,5 +1,64 @@
 
     // get the user inputs
-    var value1 = prompt('enter first value');
-    var value2 = prompt('enter the second value');
-    var choice = prompt('Choose your action (a)dd (s)ubtract (m)ultiply (d)ivide') || "a";
+var calcChoice = prompt("Which calculator do you want to use?  (B)asic | (A)dvanced")
+
+if(calcChoice == "b")
+{
+  var value1 = parseInt(prompt('enter first value'));
+  var value2 = parseInt(prompt('enter the second value'));
+  var choice = prompt('Choose your action (a)dd (s)ubtract (m)ultiply (d)ivide (Mo)dulus') || "a";
+
+//Basic Calculator
+  if(choice == "a")
+  {
+    Addition();
+  }
+  else if(choice == "s")
+  {
+    Subtraction();
+  }
+  else if(choice == "d")
+  {
+    Division();
+  }
+  else if(choice == "m")
+  {
+    Multiplication();
+  }
+  else if(choice == "mo")
+  {
+    Modulus();
+  }
+  else
+  {
+    console.log("Not a valid input.");
+  }
+}
+function Modulus()
+{
+  console.log(value1 % value2);
+}
+function Addition()
+{
+  console.log(value1 + value2);
+}
+function Subtraction()
+{
+  console.log(value1 - value2);
+}
+function Division()
+{
+  console.log(value1 / value2);
+}
+function Multiplication()
+{
+  console.log(value1 * value2);
+}
+function Power()
+{
+  console.log(Math.pow(value1, value2));
+}
+function SquareRoot()
+{
+  console.log(Math.sqrt(value1));
+}
